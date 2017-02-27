30979d1ae8da02092ed7e5df81e8710ab071ba44
Shiryaev pavel <pavelshir97@gmail.com>
2017-02-27 21:20:56 +0300

    hw1

--- homeworks/simple_tasks/bin/03.pl
+++ homeworks/simple_tasks/bin/03.pl
@@ -1,38 +1,50 @@
 #!/usr/bin/perl
 
+use warnings;
+use 5.010;
 use strict;
 use warnings;
 
 =encoding UTF8
 =head1 SYNOPSYS
-
 Поиск наименьшего и наибольшего из 3-х чисел
-
 =head1 run ($x, $y, $z)
-
 Функция поиска минимального и максимального из 3-х чисел $x, $y, $z.
 Пачатает минимальное и максимальное числа, в виде "$value1, $value2\n"
-
 Примеры: 
-
 run(1, 2, 3) - печатает "1, 3\n".
-
 run(1, 1, 1) - печатает "1, 1\n"
-
 run(1, 2, 2) - печатает "1, 2\n"
-
 =cut
+
+sub mn {
+    my ($x, $y) = @_;
+    if ($x < $y) {
+        return $x;
+    } else {
+        return $y;
+    }
+}
+
+sub mx {
+    my ($x, $y) = @_;
+    if ($x > $y) {
+        return $x;
+    } else {
+        return $y;
+    }
+}
 
 sub run {
     my ($x, $y, $z) = @_;
     my $min = undef;
     my $max = undef;
-
-    # ...
-    # Вычисление минимума и максимума
-    # ...
+    
+    $min = mn($x, mn($y, $z));
+    $max = mx($x, mx($y, $z));
 
     print "$min, $max\n";
 }
 
+run (-3, -5, -2);
 1;
--- homeworks/simple_tasks/bin/05.pl
+++ homeworks/simple_tasks/bin/05.pl
@@ -1,40 +1,37 @@
 #!/usr/bin/perl
 
+use 5.010;
 use strict;
 use warnings;
 
 =encoding UTF8
 =head1 SYNOPSYS
-
 Поиск количества вхождений строки в подстроку.
-
 =head1 run ($str, $substr)
-
 Функция поиска количества вхождений строки $substr в строку $str.
 Пачатает количество вхождений в виде "$count\n"
 Если вхождений нет - печатает "0\n".
-
 Примеры: 
-
 run("aaaa", "aa") - печатает "2\n".
-
 run("aaaa", "a") - печатает "4\n"
-
 run("abcab", "ab") - печатает "2\n"
-
 run("ab", "c") - печатает "0\n"
-
 =cut
 
 sub run {
     my ($str, $substr) = @_;
     my $num = 0;
-
-    # ...
-    # Вычисление количества вохождений строки $substr в строку $str,
-    # ...
+    my $len1 = length($str);
+    my $len2 = length($substr);
+    for (my $i = 0; $i < $len1; $i++) {
+        if (substr($str, $i, $len2) eq $substr) {
+            $num++;
+        }
+    }
 
     print "$num\n";
 }
+
+run('e#abab#deab', 'ab');
 
 1;
